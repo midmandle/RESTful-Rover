b0222192e0f583a97363a4698e2485da166979ed
Rebeca Gonzalez <bequipedia@gmail.com>
2023-06-15 11:54:07 +0200

    pass failing test

--- src/main/java/org/example/controllers/RoverController.java
+++ src/main/java/org/example/controllers/RoverController.java
@@ -1,12 +1,17 @@
 package org.example.controllers;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
 import org.example.services.RoverService;
 import spark.Request;
 import spark.Response;
 
 public class RoverController {
+    private final RoverService roverService;
+
     public RoverController(RoverService roverService) {
 
+        this.roverService = roverService;
     }
 
     public String getRoverHandler(Request request, Response response) {
@@ -14,7 +19,10 @@ public class RoverController {
     }
 
     public String createRoverHandler(Request request, Response response) {
-        throw new UnsupportedOperationException();
+        JsonObject body = Json.parse(request.body()).asObject();
+        String id = body.getString("id", null);
+        roverService.create(id);
+        return "";
     }
 
     public String moveRoverHandler(Request request, Response response) {
